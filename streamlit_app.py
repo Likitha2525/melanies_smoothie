72c22ec9e40c884b99d8b86634fa00b3da301c1a
Likitha2525 <likitha.buggana2525@gmail.com>
2025-02-02 03:52:56 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ if ingredients_list :
 
     ingredients_string = ''
 
-    for fruit_chosen in ingredients_list:
+     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ''
         
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
@@ -48,12 +48,12 @@ if ingredients_list :
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     #st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order,orders_filled)
-                values (' """ + ingredients_string+ """',' """+ name_on_order+"""')"""
+                     values (' """ + ingredients_string+ """',' """+ name_on_order+"""')"""
    
     st.write(my_insert_stmt)
     
     time_to_insert = st.button('Submit order')
     if time_to_insert:
-                     session.sql(my_insert_stmt).collect()
+                  session.sql(my_insert_stmt).collect()
 
-                     st.success('Your Smoothie is ordered!', icon="✅")
+    st.success('Your Smoothie is ordered!', icon="✅")
