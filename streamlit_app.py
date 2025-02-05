46b437107efec1296a7fe3e8cd918eb9de7c6250
Likitha2525 <likitha.buggana2525@gmail.com>
2025-02-05 23:30:49 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,11 +45,10 @@ if ingredients_list:
            fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
   #st.write(ingredients_string)
-time_to_insert = st.button('Submit order')
-    if time_to_insert:
-                  session.sql(my_insert_stmt).collect()
-
-    st.success('Your Smoothie is ordered!', icon="✅")
+  time_to_insert = st.button('Submit order')
+      if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
 
  # my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
                    #values ('""" + ingredients_string + """')"""
